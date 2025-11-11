ac733a2b433a1f02841b0ccb4a6139cbcef51243
Ulises Miguel Alvarez Julian <ulimigue44@gmail.com>
2025-11-10 21:13:37 -0600

    Update Login.kt

--- app/src/main/java/com/example/examen_app/ui/pages/Login.kt
+++ app/src/main/java/com/example/examen_app/ui/pages/Login.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign // <-- AÑADE ESTA IMPORTACIÓN
+import androidx.compose.ui.text.style.TextAlign 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.examen_app.R
